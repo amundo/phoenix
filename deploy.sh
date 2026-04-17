41a0428dfb756775e753cac1a8ab07a8e6358754
Patrick Hall <pathall@gmail.com>
2026-04-17 11:23:41 -0600

    refactor: Update emoji catalog by removing massage-related emojis and adjust emoji catalog fetch path in editor

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 m2h --section-divs=true
 generate-toc
+rsync --delete --exclude-from=exclude-from.txt  -rtpzv /Users/pathall/Sites/Games/phoenix/ ruphusco@ruphus.com:ruphus.com/game/
 rsync --delete --exclude-from=exclude-from.txt  -rtpzv /Users/pathall/Sites/Games/phoenix/ ruphusco@ruphus.com:ruphus.com/phoenix/
